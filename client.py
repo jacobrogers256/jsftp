ead582f15dc07f5f7086e2dfa0099b8817114712
Jacobo <jacobrogers256@gmail.com>
2023-05-02 16:00:55 -0500

    Add files via upload

---
+++ client.py
@@ -0,0 +1,54 @@
+import socket, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--address', type=str,
+			default="0.0.0.0",
+			help='IP Address to connect to')
+parser.add_argument('-p', '--port', type=int, default=65432,
+			help='TCP port to listen on')
+args = parser.parse_args()
+
+file_contents = bytes()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.connect((args.address, args.port))
+
+	while True:
+		comm = input("Command ?")
+		s.sendall(comm.encode('utf-8'))
+		if(("list" in comm.lower()) or ("ls" in comm.lower())):
+			sbuf = ""
+			l = int(s.recv(64).decode('utf-8'))
+			s.sendall(b"ACK")
+			while len(sbuf) < l:
+				sbuf += s.recv(1028).decode('utf-8')
+				print(sbuf)
+		elif("get" in comm.lower()):
+			file_contents = bytes()
+			file_name = input("Enter File Name> ")
+			s.sendall(bytes(file_name, 'utf-8'))
+			header_data = s.recv(1024).decode(encoding='utf-8')
+			try:
+				file_len = int(header_data)				
+			except:
+				print("File does not exist on server.")
+			try:
+				s.sendall(b'ACK')
+				while len(file_contents) < file_len:
+					file_contents += s.recv(1024)
+					print(len(file_contents) / file_len)
+				with open(file_name, "wb") as f:
+					f.write(file_contents)
+			except:
+				print(end='')
+		elif(("exit" in comm.lower()) or ("quit" in comm.lower())):
+			print("Goodbye!")
+			exit()
+		elif(("?" in comm.lower()) or ("help" in comm.lower())):
+			print("COMMANDS:\nLS/LIST - LIST FILES")
+			print("GET - GET FILE FROM SERVER")
+			print("CD/CHDIR - CHANGE DIRECTORY")
+			print("EXIT/QUIT - TERMINATE CONNECTION")
+		elif(("cd" in comm.lower()) or ("chdir" in comm.lower())):
+			dir = input("Enter directory to enter> ")
+			s.sendall(bytes(dir, 'utf-8'))
---
+++ server.py
@@ -0,0 +1,46 @@
+import socket, argparse, os
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-a', '--address', type=str,
+			default="0.0.0.0",
+			help='IP Address to listen on')
+parser.add_argument('-p', '--port', type=int, default=65432,
+			help='TCP port to send on')
+args = parser.parse_args()
+
+orig_dir = os.getcwd()
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+	s.bind((args.address, args.port))
+	s.listen()
+	print(f"Listening on {args.address}:{args.port}...")
+	while True:
+		conn, addr = s.accept()
+		with conn:
+			print(f"Connected by {addr}")
+			while True:
+				cmd = conn.recv(1024).decode("utf-8")
+				if(("list" in cmd.lower()) or ("ls" in cmd.lower())):
+					files = "File Listing:\n"
+					files += "\n".join([str("\t" + f) for f in os.listdir('.') if os.path.isfile(f)]) + "\n"
+					files += "\n".join([str("\t" + f.path) for f in os.scandir(".") if f.is_dir()])
+					conn.sendall(bytes(str(len(files)), 'utf-8'))
+					conn.recv(3)
+					conn.sendall(files.encode('utf-8'))
+				elif("get" in cmd.lower()):
+					data = conn.recv(1024).decode(encoding="utf-8")
+					try:
+						with open(data, 'rb') as f:
+							file_contents = f.read()
+							file_len = len(file_contents)
+							header = bytes(str(file_len), 'utf-8')
+						conn.sendall(header)
+						conn.recv(3)
+						conn.sendall(file_contents)
+					except:
+						conn.sendall("Resource Not Found!".encode("utf-8"))
+				elif(("exit" in cmd.lower()) or ("quit" in cmd.lower())):
+					os.chdir(orig_dir)
+					break
+				elif(("cd" in cmd.lower()) or ("chdir" in cmd.lower())):
+					os.chdir(conn.recv(1024).decode('utf-8'))
